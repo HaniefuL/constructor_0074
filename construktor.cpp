4fe185382e2ad85890d2b5a4428b436229a6dff5
HaniefuL <haniefulgeofarry@gmail.com>
2024-05-13 21:27:04 +0700

    menuliskan int main

--- construktor.cpp
+++ construktor.cpp
@@ -2,4 +2,35 @@
 #include <string>
 using namespace std;
 
-class Mahasiswa{};
+class Mahasiswa{
+    public:
+        int nim;
+        string nama;
+
+    Mahasiswa(){
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim){
+        nim = iNim;
+    };
+    Mahasiswa(string iNama);
+    Mahasiswa(int iNim, string iNama);
+    void cetak(){
+        cout << "NIM    = " << nim << endl;
+        cout << "Nama   = " << nama << endl;
+        cout << endl;
+    };
+};
+
+Mahasiswa::Mahasiswa(string iNama){
+    nim = 0;
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama){
+    nim = iNim;
+    nama = iNama;
+}
+
+int main(){}
