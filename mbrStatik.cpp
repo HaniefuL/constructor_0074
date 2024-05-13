c3d01893d659afbe37d308ff0099216cba8b34c3
HaniefuL <haniefulgeofarry@gmail.com>
2024-05-13 19:54:40 +0700

    menuliskan int Mahasiswa

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa {}
+class Mahasiswa {
+    public:
+        static int nim;
+        int id;
+        string nama;
+
+        void setID();
+
+        void printAll();
+        Mahasiswa(string pnama) :nama(pnama){
+            setID();
+        }
+};
+
+int Mahasiswa::nim = 20;
